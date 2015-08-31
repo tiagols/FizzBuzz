bfe7baa7e1019d3ff45c2c7a3503518744235250
Tiago Santos <tiagols@gmail.com>
2015-08-31 01:10:05 -0500

    v1
    done

---
+++ FizzBuzz/Input.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Input.aspx.cs" Inherits="FizzBuzz.Input" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<h1>FizzBuzz</h1>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblLower" runat="server" Text="Lower:"></asp:Label>
+        <asp:TextBox ID="txtLower" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblHigher" runat="server" Text="Higher:"></asp:Label>
+        <asp:TextBox ID="txtHigher" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Value:
+        <asp:TextBox ID="txtValue" runat="server"></asp:TextBox>
+        <asp:Button ID="btnAdd" runat="server" onclick="btnAdd_Click" Text="Add" />
+        <br />
+        <br />
+        <asp:Label ID="lblErro" runat="server" ForeColor="#FF0066"></asp:Label>
+        <br />
+    
+    </div>
+    <asp:GridView ID="gvResultado" runat="server" AutoGenerateColumns="False" 
+        CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:BoundField DataField="value" HeaderText="Value" />
+            <asp:BoundField DataField="result" HeaderText="Result" />
+        </Columns>
+        <EditRowStyle BackColor="#7C6F57" />
+        <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#E3EAEB" />
+        <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F8FAFA" />
+        <SortedAscendingHeaderStyle BackColor="#246B61" />
+        <SortedDescendingCellStyle BackColor="#D4DFE1" />
+        <SortedDescendingHeaderStyle BackColor="#15524A" />
+    </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ FizzBuzz/Input.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FizzBuzz
+{
+    public class ValueResult
+    {
+        public Double value { get; set; }
+      
+        public string result { get; set; }
+
+        public ValueResult(double val, string res)
+        {
+            value = val;
+            result = res;
+        }
+
+        public ValueResult()
+        {
+           
+        }
+    }
+    public partial class Input : System.Web.UI.Page
+    {
+        List<ValueResult> objs;
+        Double higher = 0;
+        Double lower = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErro.Text = string.Empty;
+            if (!IsPostBack)
+            {
+                objs = new List<ValueResult>();
+                Session["objs"] = objs;
+            }
+            else
+            {
+                objs = (List<ValueResult>)Session["objs"];
+                validateFields();
+            }
+        }
+
+        private bool validateFields()
+        {
+            bool valid = true;
+            string msg = string.Empty;
+            if (!Double.TryParse(txtHigher.Text, out higher))
+            {
+                msg = "Higher is not a number, ";
+            }
+            if (!Double.TryParse(txtLower.Text, out lower))
+            {
+                msg += "Lower is not a number, ";
+            }
+            if(!msg.Equals(string.Empty))
+            {
+                valid = false;
+            }
+            lblErro.Text = msg;
+            return valid;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Double value = 0;
+            string result = string.Empty;
+
+            if (validateFields())
+            {
+
+                if (Double.TryParse(txtValue.Text, out value))
+                {
+                    ValueResult vr = new ValueResult();
+                    if ((value % lower) == 0)
+                        result = "Fizz";
+
+                    if ((value % higher) == 0)
+                        result += "Buzz";
+
+                    if (result.Equals(string.Empty))
+                        result = "N/A";
+
+                    vr.value = value;
+                    vr.result = result;
+                    objs.Add(vr);
+                    Session["objs"] = objs;
+                }
+                else
+                {
+                    lblErro.Text = "Value is not a Number.";
+                }
+            }
+                bindGVResult();
+            
+        }
+
+        public void bindGVResult()
+        {
+            gvResultado.DataSource = objs;
+            gvResultado.DataBind();
+        }
+    }
+}
